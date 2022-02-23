acc64e24e0d1cc77466574224d893a237bab839d
vanhuycx <vanhuycx@gmail.com>
2022-02-22 20:58:02 -0500

    Change the page header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import Theme from '../styles/theme';
+import Head from 'next/head';
+
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
+      <Head>
+        <title>Van Pham's Portfolio</title>
+        <meta name="description" content="Meta description for the portfolio" />
+      </Head>
         <Component {...pageProps} />
       </Theme>
     </>
